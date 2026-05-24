0286b1b94e37a42ef44df795ea9529810229d1cf
Dancan1907 <dancankalerwa@gmail.com>
2026-05-25 00:29:00 +0300

    09_group_by

---
+++ part3/09_group_by.sql
@@ -0,0 +1,18 @@
+
+--Creates groups of rows
+--WHERE -> filters rows before grouping
+--HAVING filters groups after grouping
+
+--find authors who have written atleast 2 posts 
+
+SELECT 
+ u.name AS author_name,
+ COUNT(p.id) AS total_posts,
+ SUM(p.views) AS total_views
+FROM users  AS u 
+LEFT JOIN posts as p 
+ ON u.id = p.user_id
+GROUP BY u.id, u.name
+HAVING COUNT(p.id) >= 1
+ORDER BY total_posts DESC;
+
